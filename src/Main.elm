f0c5dbafa8cf32c658aef1961f525e88a00a5122
Mathieu Agopian <mathieu@agopian.info>
2020-03-15 11:17:29 +0100

    Extract the grid template columns calculation in its own function

--- src/Main.elm
+++ src/Main.elm
@@ -14,9 +14,7 @@ main =
         , Html.Attributes.style "height" "500px"
         , Html.Attributes.style "width" "500px"
         , Html.Attributes.style "display" "grid"
-        , Html.Attributes.style
-            "grid-template-columns"
-            ("repeat(" ++ String.fromInt gridWidth ++ ", 1fr)")
+        , Html.Attributes.style "grid-template-columns" <| gridTemplateColumns gridWidth
         , Html.Attributes.style "grid-auto-rows" "1fr"
         , Html.Attributes.style "grid-gap" "1px"
         , Html.Attributes.style "padding" "1px"
@@ -31,3 +29,8 @@ main =
         , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
         , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
         ]
+
+
+gridTemplateColumns : Int -> String
+gridTemplateColumns width =
+    "repeat(" ++ String.fromInt width ++ ", 1fr)"
