b2bcc1d4ab659f7cd54407af22988d79a0e634cf
Mathieu Agopian <mathieu@agopian.info>
2020-03-15 11:05:59 +0100

    Display a grid of 3x3 cells (naive)

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,27 @@
 module Main exposing (main)
 
 import Html
+import Html.Attributes
 
 
 main =
-    Html.text "Hello world!"
+    Html.div
+        [ Html.Attributes.style "background-color" "#000"
+        , Html.Attributes.style "height" "500px"
+        , Html.Attributes.style "width" "500px"
+        , Html.Attributes.style "display" "grid"
+        , Html.Attributes.style "grid-template-columns" "repeat(3, 1fr)"
+        , Html.Attributes.style "grid-auto-rows" "1fr"
+        , Html.Attributes.style "grid-gap" "1px"
+        , Html.Attributes.style "padding" "1px"
+        ]
+        [ Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        , Html.div [ Html.Attributes.style "background-color" "#fff" ] []
+        ]
