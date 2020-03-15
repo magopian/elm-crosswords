6a270fb68e3fe15750bf8cb0a2e7a26f086bfcde
Mathieu Agopian <mathieu@agopian.info>
2020-03-15 11:22:49 +0100

    gridTemplateColumns function now returns an Html.Attribute

--- src/Main.elm
+++ src/Main.elm
@@ -14,7 +14,7 @@ main =
         , Html.Attributes.style "height" "500px"
         , Html.Attributes.style "width" "500px"
         , Html.Attributes.style "display" "grid"
-        , Html.Attributes.style "grid-template-columns" <| gridTemplateColumns gridWidth
+        , gridTemplateColumns gridWidth
         , Html.Attributes.style "grid-auto-rows" "1fr"
         , Html.Attributes.style "grid-gap" "1px"
         , Html.Attributes.style "padding" "1px"
@@ -31,6 +31,8 @@ main =
         ]
 
 
-gridTemplateColumns : Int -> String
+gridTemplateColumns : Int -> Html.Attribute ()
 gridTemplateColumns width =
-    "repeat(" ++ String.fromInt width ++ ", 1fr)"
+    Html.Attributes.style
+        "grid-template-columns"
+        ("repeat(" ++ String.fromInt width ++ ", 1fr)")
