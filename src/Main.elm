5ec14367f2634cb55bdf33e01aa6d0eee6e93197
Mathieu Agopian <mathieu@agopian.info>
2020-03-15 11:13:25 +0100

    Define the grid-template-columns using a gridWidth variable

--- src/Main.elm
+++ src/Main.elm
@@ -4,13 +4,19 @@ import Html
 import Html.Attributes
 
 
+gridWidth =
+    3
+
+
 main =
     Html.div
         [ Html.Attributes.style "background-color" "#000"
         , Html.Attributes.style "height" "500px"
         , Html.Attributes.style "width" "500px"
         , Html.Attributes.style "display" "grid"
-        , Html.Attributes.style "grid-template-columns" "repeat(3, 1fr)"
+        , Html.Attributes.style
+            "grid-template-columns"
+            ("repeat(" ++ String.fromInt gridWidth ++ ", 1fr)")
         , Html.Attributes.style "grid-auto-rows" "1fr"
         , Html.Attributes.style "grid-gap" "1px"
         , Html.Attributes.style "padding" "1px"
